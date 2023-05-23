e6a766181b1dbb0ccd95d1af405b23ce2a2639eb
Ntheus <matheusoliveira.7313@gmail.com>
2023-05-23 19:05:26 -0300

    data

--- src/Negocio/Genius.java
+++ src/Negocio/Genius.java
@@ -1,18 +1,20 @@
 package Negocio;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.List;
 
 public class Genius {
-    private int data;// mudar para tipo para Date;
+    private Data data;// mudar para tipo para Date;
     private String titulodoCampeonato;
     private String difuculdade;
     private List<Jogador> jogadores; // para facilitar a alteracao na quantidade de jogadores
     private List<Integer> sequenciadeCores;
     private int tempoParaReagir; // A definir oq poderia ser considerado facil ou dificil
 
-    public Genius(int data, String titulodoCampeonato, String dificuldade) {
-        this.data = data;
+    public Genius(String titulodoCampeonato, String dificuldade) {
+        data = new Data(LocalDateTime.now().getDayOfMonth(), LocalDateTime.now().getMonthValue(),
+                LocalDateTime.now().getYear());
         this.titulodoCampeonato = titulodoCampeonato;
         this.difuculdade = dificuldade;
     }
@@ -41,6 +43,10 @@ public class Genius {
             // return true
         }
         return false;
+    }
+
+    public Data geData() {
+        return this.data;
     }
 
     public boolean acertouaSequencia(int numerodaJogada, int codigodaCor) {
--- src/View/App.java
+++ src/View/App.java
@@ -1,7 +1,26 @@
 package View;
 
+import java.time.Clock;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Clock clock = Clock.systemDefaultZone();
+        Calendar datadodia = Calendar.getInstance(TimeZone.getTimeZone("GMT-3"));
+
+        System.out.println("Antes" + clock.millis() % 10000);
+        long x = clock.millis() % 10000 + 1000;
+        Clock relogio = clock.systemDefaultZone();
+        System.out.println(LocalDateTime.now().getYear()/* relogio.millis() % 10000 + ">?" + x */);
+        /*
+         * while (clock.millis() <= x) {
+         * System.out.println(clock.millis());
+         * }
+         */
+        // System.out.println();
     }
 }
