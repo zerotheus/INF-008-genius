cf382e26f39606d7b30b3bc182a4611b12b7d4c6
Ntheus <104659816+zerotheus@users.noreply.github.com>
2023-05-25 16:43:49 -0300

    enum dificuldade

--- src/Enums/Cor.java
+++ src/Enums/Cor.java
@@ -1,8 +1,8 @@
 package Enums;
 
 public enum Cor {
-    // da esquerda para a direita,0,1,2 e 3;
-    VERMELHO, VERDE,
-    AZUL, AMARELO;
-
+    vermelho,
+    verde,
+    azul,
+    amarelo
 }
---
+++ src/Enums/Dificuldade.java
@@ -0,0 +1,7 @@
+package Enums;
+
+public enum Dificuldade {
+	facil,
+	medio,
+	dificil
+}
