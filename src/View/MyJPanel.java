f600e30a14bf1b04e3f74469fd678d85939126c4
Ntheus <matheusoliveira.7313@gmail.com>
2023-06-07 03:17:47 -0300

    removido imports de audio

--- src/View/MyJPanel.java
+++ src/View/MyJPanel.java
@@ -1,13 +1,6 @@
 package View;
 
 import java.io.File;
-import java.io.IOException;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JPanel;
 
 public abstract class MyJPanel extends JPanel {
