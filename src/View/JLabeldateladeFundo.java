39a7f3129127d050a90c3433364b71be4f0461ea
Ntheus <matheusoliveira.7313@gmail.com>
2023-06-07 03:06:16 -0300

    autoformat

--- src/View/JLabeldateladeFundo.java
+++ src/View/JLabeldateladeFundo.java
@@ -9,7 +9,7 @@ public class JLabeldateladeFundo extends JLabel {
     private final String basePath;
     private final String imagesBasePath;
 
-    JLabeldateladeFundo() {
+    public JLabeldateladeFundo() {
         basePath = new File("").getAbsolutePath() + "\\";
         imagesBasePath = basePath + new File("src\\imagens").getPath() + "\\";
         this.setIcon(new ImageIcon(this.imagesBasePath + "tela.png"));
