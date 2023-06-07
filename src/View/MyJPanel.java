f816ac18b05cec297f24b383f2449486116f7df7
Ntheus <matheusoliveira.7313@gmail.com>
2023-06-07 02:50:39 -0300

    metodos de som movidos para myJlabelWithSound

--- src/View/MyJPanel.java
+++ src/View/MyJPanel.java
@@ -14,31 +14,11 @@ public abstract class MyJPanel extends JPanel {
 
     private final String basePath;
     private final String imagesBasePath;
-    private final String sfxBotaoMenuBasePath;
-    private AudioInputStream audioInputStream;
-	private Clip clip;
 
     protected MyJPanel() {
         this.imagesBasePath = new File("src\\imagens").getPath() + "\\";
-        this.sfxBotaoMenuBasePath = new File("src\\sfx").getPath() +"\\MenuBotao.Wav";
         this.basePath = new File("").getAbsolutePath() + "\\";
-        
-    }
-    
-    protected void StartSound(String URL) {
-    	try {
-			audioInputStream = AudioSystem.getAudioInputStream(new File((URL)));//"/sfx/MenuBotão.wav" 
-			clip = AudioSystem.getClip();
-			clip.open(audioInputStream);
-		} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1 ) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		clip.start();
-    }
-    protected void StopSound() {
-    	clip.stop();
-    	clip.close();
+
     }
 
     protected String getImagesPath() {
