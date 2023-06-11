e02a275ba7041d2fa79478f605955bd9f95d6659
Ntheus <matheusoliveira.7313@gmail.com>
2023-06-11 05:22:49 -0300

    sysout para verificar funcionamento

--- src/View/KeyButtonMaps.java
+++ src/View/KeyButtonMaps.java
@@ -1,15 +1,20 @@
 package View;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.AbstractAction;
 import javax.swing.JTabbedPane;
+
+import View.geniusLabels.GeniusLabels;
 
 public class KeyButtonMaps extends AbstractAction {
 
     private final JTabbedPane tabbedPane;
     private final GeniusLabels botao;
-    private MyJPanel panel;
+    private Thread thread;
 
     KeyButtonMaps(JTabbedPane tabbedPane, GeniusLabels botao, MyJPanel panel) {
         this.tabbedPane = tabbedPane;
@@ -18,7 +23,12 @@ public class KeyButtonMaps extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        this.botao.pisca();
+        try {
+            this.botao.pisca();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
+            e1.printStackTrace();
+        }
+        System.out.println("pisquei");
     }
 
 }
