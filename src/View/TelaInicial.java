419d2e48b091cd60e758fd27fcea2cebf6641467
Ntheus <matheusoliveira.7313@gmail.com>
2023-06-14 13:07:45 -0300

    removido o botao de voltar

--- src/View/TelaInicial.java
+++ src/View/TelaInicial.java
@@ -20,12 +20,6 @@ public class TelaInicial extends MyJPanel {
         lblbutao.setBounds(587, 573, 276, 117);
         this.add(lblbutao);
 
-        MyJLabelwithSound lblFechar = new MyJLabelwithSound();
-        lblFechar.setIcon(new ImageIcon(this.getImagesPath() + "botaoVoltar.png"));
-        lblFechar.setBounds(650, 795, 106, 53);
-        lblFechar.setVisible(true);
-        this.add(lblFechar);
-
         lbltelaFundo = new JLabeldateladeFundo();
         this.add(lbltelaFundo);
 
@@ -40,8 +34,6 @@ public class TelaInicial extends MyJPanel {
                 } catch (Exception e1) {
                     System.out.println(e.toString());
                 }
-                lblbutao.setEnabled(false);
-                lblbutao.setVisible(false);
                 JPanel teladeMododeJogo = new TeladeSelecaoModo(tabbedPane);
                 tabbedPane.insertTab("Selecione o modo de Jogo", null, teladeMododeJogo, TOOL_TIP_TEXT_KEY, 1);
                 tabbedPane.removeTabAt(0);
@@ -50,23 +42,6 @@ public class TelaInicial extends MyJPanel {
 
         lblbutao.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke('.'), "A");
         lblbutao.getActionMap().put("A", new AcaoTrocaparaTeladoJogo(tabbedPane, lblbutao));
-
-        lblFechar.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (e.getSource() != lblFechar) {
-                    return;
-                }
-                try {
-                    lblFechar.startSound();
-                } catch (Exception e1) {
-                    System.out.println(e1.toString());
-                }
-
-                System.exit(0);
-
-            }
-        });
 
     }
 
